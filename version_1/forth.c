35f05f8787f7ad5bf9ef4fbeb9021daade11028d
Tobias Frejo <tobias@frejo.dk>
2020-11-23 10:53:33 +0100

    Small fixes

--- version_1/forth.c
+++ version_1/forth.c
@@ -1,4 +1,5 @@
 /// @version 1.0
+#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h> //atoi
@@ -20,7 +21,7 @@
 
 typedef struct built_in_word_dict
 {
-    built_in_word* word;
+    built_in_word (* word);
     int length;
 } built_in_word_dict;
 
@@ -45,15 +46,15 @@ built_in_word_dict* initialize_built_in_dict(void) {
     built_in_word_dict* dict = malloc(sizeof(built_in_word_dict));
     dict->length = WORDS;
     dict->word = calloc(WORDS, sizeof(built_in_word));
-    dict->word[0] = {'+', add, 2};
-    dict->word[1] = {'-', minus, 2};
-    dict->word[2] = {'*', mult, 2};
-    dict->word[3] = {'/', divide,2};
-    dict->word[4] = {'~', uniminus, 1};
+    dict->word[0] = (built_in_word){'+', add, 2};
+    dict->word[1] = (built_in_word){'-', minus, 2};
+    dict->word[2] = (built_in_word){'*', mult, 2};
+    dict->word[3] = (built_in_word){'/', divide,2};
+    dict->word[4] = (built_in_word){'~', uniminus, 1};
 
     /* Verify initialization */
     for (int i = 0; i < WORDS; i++) {
-        assert(dict->word[i] != 0);
+        assert(dict->word[i].operator != 0 && dict->word[i].get_result != 0);
     }
 
     return dict;
