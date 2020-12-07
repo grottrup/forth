6f5e9edfe9bd2f176e7aaa75bdb9dd262d9fb937
grottrup <grottrup@protonmail.com>
2020-12-07 10:59:37 +0100

    start implementing

--- version_2/operations.c
+++ version_2/operations.c
@@ -1,49 +1,68 @@
 #include "operations.h"
 
-void add(stack* s){
+void Add(stack* s){
     int b = pop(s);
     int a = pop(s);
     push(s,a+b);
 }
 
-void minus(stack* s){
+void Minus(stack* s){
     int b = pop(s);
     int a = pop(s);
     push(s,a-b);
 }
 
-void mult(stack* s){
+void Mult(stack* s){
     int b = pop(s);
     int a = pop(s);
     push(s,a*b);
 }
 
-void divide(stack* s){
+void Divide(stack* s){
     int b = pop(s);
     int a = pop(s);
     push(s,a/b);
 }
 
+void Emit(stack* s){
+
+}
+
+void CR(stack* s);
+
+void Dot(stack* s) {
+    int a = pop(s);
+    printf("%d", a);
+}
+
+void Qmark(stack* s);
+void Dup(stack* s);
+
+void Drop(stack* s)
+{
+    pop(s);
+}
+void Swap(stack* s)
+{
+    int b = pop(s);
+    int a = pop(s);
+    push(s,b);
+    push(s,a);
+}
+
+void Over(stack* s);
+void Rot(stack* s);
+void And(stack* s);
+void Or(stack* s);
+void Not(stack* s);
+void If(stack* s);
+void Else(stack* s);
+void Then(stack* s);
+
+
+
+
 // int uniminus(int* arr){
 //     return -arr[0];
 // }
 
-// int emit(int* arr)
-// {
-//     return 0;
-// }
-
-// int dot(stack* s)
-// {
-//     return 0;
-// }
-
-// int cr(int* arr)
-// {
-//     return 0;
-// }
-
-// int qmark(int* arr)
-// {
-//     return 0;
-// }
--- version_2/operations.h
+++ version_2/operations.h
@@ -2,14 +2,25 @@
 #define _OPERATIONS_H_
 #include "stack.h"
 
-void add(stack* s);
-void minus(stack* s);
-void mult(stack* s);
-void divide(stack* s);
+void Add(stack* s);
+void Minus(stack* s);
+void Mult(stack* s);
+void Divide(stack* s);
 void uniminus(stack* s);
-// int emit(int* arr);
-// int dot(int* arr);
-// int cr(int* arr);
-// int qmark(int* arr);
+void Emit(stack* s);
+void CR(stack* s);
+void Dot(stack* s);
+void Qmark(stack* s);
+void Dup(stack* s);
+void Drop(stack* s);
+void Swap(stack* s);
+void Over(stack* s);
+void Rot(stack* s);
+void And(stack* s);
+void Or(stack* s);
+void Not(stack* s);
+void If(stack* s);
+void Else(stack* s);
+void Then(stack* s);
 
 #endif
