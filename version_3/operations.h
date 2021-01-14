ea894df3809c61f309584f655b638a03e8bd14c3
grottrup <41370210+grottrup@users.noreply.github.com>
2021-01-14 12:05:06 +0100

    Update operations.h

--- version_3/operations.h
+++ version_3/operations.h
@@ -2,17 +2,19 @@
 #define _OPERATIONS_H_
 #include "stack.h"
 
-/* Data manipulation */
+/* Arithmetics */
 void Add(stack* s);
 void Minus(stack* s);
 void Mult(stack* s);
 void Divide(stack* s);
 
+/* Output */
 void Emit(stack* s);
 void CR(stack* s);
 void Dot(stack* s);
 void Qmark(stack* s);
 
+/* Data Stack */
 void Dup(stack* s);
 void Drop(stack* s);
 void Swap(stack* s);
