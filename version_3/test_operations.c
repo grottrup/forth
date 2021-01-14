a590387450f7f69bf8bc63d25f88dd703acf9b0a
grottrup <41370210+grottrup@users.noreply.github.com>
2021-01-14 12:59:59 +0100

    Update test_operations.c

--- version_3/test_operations.c
+++ version_3/test_operations.c
@@ -6,44 +6,44 @@
 void test_arithmetics(void) // white box
 {
     {
-        stack s = initialize_stack(); // whoops not so unifor implementation
+        stack s = initialize_stack();
         push(&s, 1);
         push(&s, 2);
         command w = (command){"+", Add};
         w.execute(&s);
         assert(pop(&s) == 3 );
         assert(stack_is_empty(&s));
-        free_stack(s);
+        free_stack(&s);
     }    
     {
-        stack s = initialize_stack(); // whoops not so unifor implementation
+        stack s = initialize_stack();
         push(&s, 1);
         push(&s, 2);
         command w = (command){"-", Minus};
         w.execute(&s);
         assert(pop(&s) == 3 );
         assert(stack_is_empty(&s));
-        free_stack(s);
+        free_stack(&s);
     }    
     {
-        stack s = initialize_stack(); // whoops not so unifor implementation
+        stack s = initialize_stack(); 
         push(&s, 1);
         push(&s, 2);
         command w = (command){"*", Mult};
         w.execute(&s);
         assert(pop(&s) == 3 );
         assert(stack_is_empty(&s));
-        free_stack(s);
+        free_stack(&s);
     }
     {
-        stack s = initialize_stack(); // whoops not so unifor implementation
+        stack s = initialize_stack();
         push(&s, 10);
         push(&s, 2);
         command w = (command){"/", Divide};
         w.execute(&s);
         assert(pop(&s) == 5 );
         assert(stack_is_empty(&s));
-        free_stack(s);
+        free_stack(&s);
     }
 }
 
