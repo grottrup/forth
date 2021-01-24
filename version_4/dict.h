fb4e193916176f424816b063212f587bde2171cf
grottrup <41370210+grottrup@users.noreply.github.com>
2021-01-24 15:12:18 +0100

    Add void argument at dictionary description

--- version_4/dict.h
+++ version_4/dict.h
@@ -6,13 +6,12 @@
 #include <string.h>
 #include "operations.h"
 
-/*
+/* @summary
     The dictionary of user defined words, implemented as a linked list.
     It contains
         - name : a string specifing the userdefined word
         - body : a string of space seperated words to be parsed when called
 */
-
 typedef struct user_dict_entry
 {
     const char* name;
@@ -20,21 +19,27 @@ typedef struct user_dict_entry
     struct user_dict_entry* next_entry;
 } user_dict_entry;
 
-user_dict_entry* user_dict_initialize();
+user_dict_entry* user_dict_initialize(void);
 void user_dict_assign(const char* name, const char* body, user_dict_entry* dict);
 void user_dict_unassign(const char* name, user_dict_entry* dict);
 bool user_dict_entry_exists(const char* name, user_dict_entry* dict);
 const char* user_dict_get_entry(const char* name, user_dict_entry* dict);
 
-
+/* @summary
+    The dictionary of the Forth language defined words. This version uses a linked structure. (Personally I think this is a violation of "seperation by concern", so I think we should later update this implementation)
+    
+    It contains
+        - name : a string specifing the defined word
+        - (*execute) : a function to execute the defined operator for the word
+*/
 typedef struct sys_dict_entry
 {
-    const char* name;
+    const char* name; 
     void (*execute)(stack*);
     struct sys_dict_entry* next_entry;
 } sys_dict_entry;
 
-sys_dict_entry* sys_dict_initialize();
+sys_dict_entry* sys_dict_initialize(void);
 void sys_dict_execute(const char* name, sys_dict_entry* dict, stack* s);
 bool sys_dict_entry_exists(const char* name, sys_dict_entry* dict);
 
