5a58a412f3364c1579d8476f5e0e472efc8401ff
grottrup <grottrup@protonmail.com>
2021-03-09 17:42:28 +0100

    Delete early versions from Token branch

--- version_1/parser.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PARSER_H_
-#define _PARSER_H_
-
-bool is_operator(char x);
-bool is_number(char *x);
-
-#endif
--- version_1/runtest.sh
+++
@@ -1,8 +0,0 @@
-# Update library
-gcc -g -c stack.c
-ar rcs libstack.a stack.o
-
-# Run tests for library
-gcc -c test_stack.c
-gcc test_stack.o -lstack -L . -o test_stack
-./test_stack
