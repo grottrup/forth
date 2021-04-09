1ae5bfb98b4619478a9a231afe5b4da19be05757
grottrup <41370210+grottrup@users.noreply.github.com>
2021-04-09 12:56:28 +0200

    Update sentence.c

--- version_5/sentence.c
+++ version_5/sentence.c
@@ -19,7 +19,7 @@ word_node* _break_string_into_tokens(char* src_cpy, char* saveptr, char* token)
         {
             strcpy(token_copy, token); // This allocates only the space needed for the token.
             token = strtok_r(NULL, " ", &saveptr); // TODO: token causes "degeneration"
-            if(token && saveptr)
+            if(token != NULL && saveptr != NULL)
             {
                 word_node* next = _break_string_into_tokens(src_cpy, saveptr, token);
                 node = word_node_ctor(token_copy, next);
