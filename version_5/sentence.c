96ac013b622bac1b09ea73fc280996244973da91
grottrup <41370210+grottrup@users.noreply.github.com>
2021-04-09 12:48:14 +0200

    Update sentence.c

--- version_5/sentence.c
+++ version_5/sentence.c
@@ -18,12 +18,10 @@ word_node* _break_string_into_tokens(char* src_cpy, char* saveptr, char* token)
         if(token_copy)
         {
             strcpy(token_copy, token); // This allocates only the space needed for the token.
-            //token = strtok_r(NULL, " ", &saveptr); // TODO: token causes "degeneration"
-            char* saveptr;
-            token = strtok_r(NULL, " ", &saveptr); // TODO: token causes "degeneration"
-            if(token)
+            char* next_token = strtok_r(NULL, " ", &saveptr); // TODO: token causes "degeneration"
+            if(next_token != NULL)
             {
-                word_node* next = _break_string_into_tokens(src_cpy, saveptr, token);
+                word_node* next = _break_string_into_tokens(src_cpy, saveptr, next_token);
                 node = word_node_ctor(token_copy, next);
             }
        
